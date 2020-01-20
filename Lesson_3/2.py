7e221fd4641fc62722a18d598e0d681aef6be95c
M01eg <ggff6665@bk.ru>
2020-01-20 04:49:32 -0800

    Update 2.py

--- Lesson_3/2.py
+++ Lesson_3/2.py
@@ -1,28 +1,20 @@
 '''
  Урок 3
- Задание 3
-В массиве случайных целых чисел поменять местами
-минимальный и максимальный элементы.
+ Задание 2
+Во втором массиве сохранить индексы четных элементов
+первого массива. Например, если дан массив со значениями
+8, 3, 15, 6, 4, 2, то во второй массив надо заполнить
+значениями 1, 4, 5, 6 (или 0, 3, 4, 5 - если индексация
+начинается с нуля), т.к. именно в этих позициях первого
+массива стоят четные числа
 '''
 
-from random import randint
+def task2():
+    first_array = [8, 3, 15, 6, 4, 2]
 
-def task3():
-    n_arr = randint(5, 10)
-    random_array = [randint(-30, 30) for i in range(n_arr)]
-
-    print(f"Сгенерирован массив:\n{random_array}")
-
-    i_min = i_max = 0
-    for i, value in enumerate(random_array):
-        if value < random_array[i_min]:
-            min_i = i
-        if value > random_array[i_max]:
-            max_i = i
-
-    random_array[i_min], random_array[i_max] = random_array[i_max], random_array[i_min]
-    print(f"После замены макс. и мин. чисел получаем:\n{random_array}")
+    print("Индексы четных символов:")
+    print([i for i, x in enumerate(first_array) if x % 2 == 0])
 
 
 if __name__ == "__main__":
-    task3()
+    task2()
