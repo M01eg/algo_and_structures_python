74b44107e97e0c0582eb3b28e595756a4ab26caf
M01eg <ggff6665@bk.ru>
2020-02-05 09:28:45 -0800

    Update 1.py

--- Lesson_5/1.py
+++ Lesson_5/1.py
@@ -1,7 +1,45 @@
-"""
-1. Пользователь вводит данные о количестве предприятий, их наименования и
-прибыль за 4 квартала (т.е. 4 отдельных числа) для каждого предприятия..
-Программа должна определить среднюю прибыль (за год для всех предприятий) и
-вывести наименования предприятий, чья прибыль выше среднего и отдельно
-вывести наименования предприятий, чья прибыль ниже среднего.
-"""
+'''
+ Урок 5
+ Задание 1
+Пользователь вводит данные о количестве предприятий,
+их наименования и прибыль за 4 квартала (т.е. 4
+отдельных числа) для каждого предприятия.. Программа
+должна определить среднюю прибыль (за год для всех
+предприятий) и вывести наименования предприятий, чья
+прибыль выше среднего и отдельно вывести наименования
+предприятий, чья прибыль ниже среднего.
+'''
+
+from collections import namedtuple
+
+
+def task1():
+    COMPANY = namedtuple('Company', 'name income')
+    companies = []
+    n_companies = int(input("Введите кол-во предприятий: "))
+    for i in range(n_companies):
+        company_name = input(f"Введите имя предприятия №{i+1}: ")
+        income = input(f"Введите прибыль этого предприятия за 4 квартала через пробел: ")
+        companies.append(COMPANY(name=company_name, income=[int(n) for n in income.split()]))
+
+    average_yearly_income = 0
+    for company in companies:
+        average_yearly_income += sum(company.income)
+
+    average_yearly_income = average_yearly_income / n_companies
+
+    print(f"\nСредняя годовая прибыль по всем: {average_yearly_income}")
+
+    print("Предприятия, заработавшие больше средней прибыли:")
+    for company in companies:
+        if sum(company.income) >= average_yearly_income:
+            print(company.name)
+
+    print("Предприятия, заработавшие меньше средней прибыли:")
+    for company in companies:
+        if sum(company.income) < average_yearly_income:
+            print(company.name)
+
+
+if __name__ == "__main__":
+    task1()
